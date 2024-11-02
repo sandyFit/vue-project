c8e931fc8a48f8621b7ef48018214644d0d5c17b
SandyFit <sandy07r@gmail.com>
2024-11-02 18:02:33 -0500

    Added CoffeePlan component

---
+++ src/components/CoffeePlan.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+        name: {type: String, default: 'Default Plan'}
+    })
+</script>
+
+<template>
+    <div class="plan">
+        <div class="description">
+            <span class="title">
+                {{ name }}
+            </span>
+        </div>
+    </div>
+</template>
---
+++ src/components/CounterButton.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0)
+</script>
+
+
+<template>
+    <button @click="count++">You clicked me {{ count }} times.</button>
+
+</template>
+
+<style>
+    button {
+        width: 20rem;
+        padding: 1rem 2rem;
+        font-size: 1rem;
+        text-transform: uppercase;
+        border-radius: 4px;
+    }
+</style>
