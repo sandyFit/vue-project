40206083fbcc7b5fccdce8de9442e9efb3406a2e
SandyFit <sandy07r@gmail.com>
2024-11-02 19:35:58 -0500

    Implemented the PlanPicker component for more reactivity

--- src/components/CoffeePlan.vue
+++ src/components/CoffeePlan.vue
@@ -1,11 +1,17 @@
 <script setup>
-defineProps({
-        name: {type: String, default: 'Default Plan'}
-    })
+import { ref } from 'vue';
+const props = defineProps({
+    name: { type: String, default: 'Default Plan' },
+    selected: {type: Boolean, default: false}
+})
+
+
 </script>
 
 <template>
-    <div class="plan">
+    <div 
+        class="plan" 
+        :class="{'active-plan' : selected}">
         <div class="description">
             <span class="title">
                 {{ name }}
---
+++ src/components/PlanPicker.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+import CoffeePlan from './CoffeePlan.vue';
+
+const plans = ref(['The Single', 'The Curious', 'The Addict', 'The Hacker'])
+
+const selectedCoffeePlan = ref()
+
+
+const handleSelectedCoffeePlan = name => {
+    selectedCoffeePlan.value = name
+}
+</script>
+
+<template>
+    <div class="plans">
+        {{ selectedCoffeePlan }}
+        <CoffeePlan v-for="plan in plans"
+            :key="plan"
+            :name="plan"
+            :selected="plan === selectedCoffeePlan"
+            @click="handleSelectedCoffeePlan(plan)">
+        </CoffeePlan>
+    </div>
+</template>
